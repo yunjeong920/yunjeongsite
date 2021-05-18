b0dcd041d91d78101ef5d54c91aa813cd62852d0
yunjeong <yunjeong920@naver.com>
2021-05-18 10:52:56 +0900

    대표 사이트 수정

--- js/common.js
+++ js/common.js
@@ -2,21 +2,24 @@ function scrollHandler() {
   if ($(window).scrollTop() >= $(".profile").position().top) {
     $(".menu-right button").css("color","#4a4a4a");
     $(".navbar img").attr("src","./img/yunlogo2.png");
-
-    $(".skill").each(function() {
-      var skill = $(this);
-      var percentage = skill.find(".percentage").text();
-      skill.find(".inner-bar").animate({width: percentage}, 1200);
-    });
   }else {
     $(".menu-right button").css("color","white");
     $(".navbar img").attr("src","./img/yunlogo.png");
   }
+
   $("section").each(function() {
     if($(window).scrollTop() >= $(this).position().top){
-      $(this).find(".vertical-center").animate({top: "0", opacity: "1"}, 800)
+      $(this).find(".page").animate({top: "0", opacity: "1"}, 800)
     }
   });
+
+  if($(window).scrollTop() >= $(".about").position().top){
+    $(".skill").each(function() {
+      var skill = $(this);
+      var percentage = skill.find(".percentage").text();
+      skill.find(".inner-bar").animate({width: percentage}, 1200);
+    });  
+  }
 }
 
 $(window).on("scroll", scrollHandler);
@@ -24,7 +27,9 @@ scrollHandler();
 //섹션 스크롤
 $(".menu-right button").on("click", function() {
   var id = $(this).attr("id");
-  if (id == "profile-btn") {
+  if (id == "header-btn") {
+    $("html, body").animate({scrollTop: $(".header").position().top}, 1000)
+  }else if(id == "profile-btn") {
     $("html, body").animate({scrollTop: $(".profile").position().top}, 1000)
   }else if (id == "about-btn") {
     $("html, body").animate({scrollTop: $(".about").position().top}, 1000)
